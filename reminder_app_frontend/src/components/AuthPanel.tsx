471154e44ca37949f291fd5a4068e0e097f94b3f
Eric Morrison <eric_m250@outlook.com>
2025-09-01 12:49:17 -0700

    cleaned up the UI in authpanel

--- reminder_app_frontend/src/components/AuthPanel.tsx
+++ reminder_app_frontend/src/components/AuthPanel.tsx
@@ -52,26 +52,26 @@ const handleLogout = async () => {
 }
 
     return(
-        <div className="bg-green-400 border-2 border-white rounded-lg p-4 mt-12 w-9/10 mx-auto">
+        <div className="w-max h-max p-2 bg-indigo-100 shadow-md rounded-lg mx-auto">
             <form 
             className="flex flex-col"
             onSubmit={
                 loggingIn? handleLogin : handleRegister
             }>
 
-                <p className="text-white">Enter a username</p>
+                <p className="text-indigo-600 inter-bold">Enter a username</p>
                 <input 
-                className="border-2 border-yellow-400 bg-white text-green-600 rounded-md"
+                className="shadow-md bg-white text-gray-600 rounded-md pl-2 mt-2"
                 type="text" 
-                placeholder="email goes here"
+                placeholder="username"
                 onChange={(e) => {
                     setUsername(e.target.value)
                 }}/>
                 {!loggingIn && 
                 <>
-                    <p className="text-white">Enter your email</p>
+                    <p className="text-indigo-600 inter-bold mt-2">Enter your email</p>
                     <input 
-                    className="border-2 border-yellow-400 bg-white text-green-600 rounded-md"
+                    className="shadow-md bg-white text-gray-600 rounded-md mt-2 pl-2"
                     type="email" 
                     placeholder="email goes here"
                     onChange={(e) => {
@@ -79,29 +79,29 @@ const handleLogout = async () => {
                     }}/>
                 </>
                 }
-                <p className="text-white">{loggingIn ? "Enter your password" : "Create a password"}</p>
+                <p className="text-indigo-600 inter-bold mt-2">{loggingIn ? "Enter your password" : "Create a password"}</p>
                 <input 
-                className="border-2 border-yellow-400 bg-white text-green-600 rounded-md"
+                className="shadow-md bg-white text-gray-600 rounded-md pl-2 mt-2"
                 type="password" 
-                placeholder="password goes here"
+                placeholder="password"
                 onChange={(e) => {
                     setPassword(e.target.value)
                 }}/>
 
                 <button type="submit"
-                className="border-2 border-yellow-400 bg-white text-green-400 p-1 rounded-md w-max mt-2">Submit</button>
+                className="bg-white px-2 rounded-md text-green-500 w-max shadow-md my-2">Submit</button>
                 
             </form>
 
             <div className="flex flex-col">
-                <p className="text-white">{loggingIn ? "Need to create an account?" : "Already a user?"}</p>
-                <button className="border-2 border-yellow-400 bg-white text-green-400 p-1 rounded-md w-max"
+                <p className="text-indigo-600 inter-bold">{loggingIn ? "Need to create an account?" : "Already a user?"}</p>
+                <button className="shadow-md bg-white text-green-500 p-1 rounded-md w-max mt-1"
                 onClick={() => {
                     setLoggingIn(prev => !prev)
                     setRegistering(prev => !prev)
                 }}>{loggingIn ? "Register" : "Login"}</button>
-                <button className="border-2 border-yellow-400 bg-white text-green-400 p-1 rounded-md w-max mt-2"
-                onClick={() => setAuthPanelOpen(false)}>Go Back</button>
+                {/* <button className="shadow-md bg-white text-green-500 p-1 rounded-md w-max mt-2"
+                onClick={() => setAuthPanelOpen(false)}>Go Back</button> */}
             </div>
         </div>
     )
--- reminder_app_frontend/src/components/Header.tsx
+++ reminder_app_frontend/src/components/Header.tsx
@@ -7,12 +7,12 @@ export function Header({ setAuthPanelOpen }: HeaderProps ) {
 
     return(
             <div className="bg-indigo-100 shadow-md rounded-lg p-4 mt-12 w-9/10 mx-auto">
-                <h2 className="inter-bold text-2xl text-indigo-600">
+                <h2 className="inter-bold text-5xl text-indigo-600">
                     Do Date
                 </h2>
-                <h4 className="open-sans-regular text-indigo-500 text-lg">Do future you a favor!</h4>
-                <button className="bg-indigo-400 px-2 rounded-md text-white w-max shadow-md my-2"
-                onClick={() => setAuthPanelOpen(true)}>Try it!</button>
+                <h4 className="open-sans-regular text-indigo-500 text-lg my-2">Do future you a favor!</h4>
+                <button className="bg-white px-2 rounded-md text-green-600 w-max shadow-md my-2"
+                onClick={() => setAuthPanelOpen(true)}>Begin</button>
             </div>
     )
 }
